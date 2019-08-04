26578074039da8312977437540aa26c5decf5d73
Hider <724598894@qq.com>
2019-08-04 14:29:14 +0800

    买家订单实体的创建

---
+++ src/main/java/com/hider/order/enums/PayStatusEnum.java
@@ -0,0 +1,17 @@
+package com.hider.order.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PayStatusEnum {
+    WAIT(1, "等待支付"),
+    SUCCESS(2, "支付成功");
+
+    private Integer code;
+    private String message;
+
+    PayStatusEnum(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+}
